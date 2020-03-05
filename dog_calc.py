05b37e2fec366367581cad4d949ad559ed573865
Keith Turner <turner@keithturner.com>
2020-03-05 07:06:32 -0700

    Update Rock

--- dog_calc.py
+++ dog_calc.py
@@ -4,4 +4,4 @@ dog_name = input ("What is your dog's name? ")
 dog_age = input ("What is your dog's age? ")
 human_age = int(dog_age) * 7
 # print (human_age)
-print ("Your dog's name is", dog_name, "and it is", human_age, "years old." )
+print ("Your dog's name is", dog_name, "and it is", human_age, "years old.")
---
+++ rock.py
@@ -0,0 +1,7 @@
+# rock, paper, scissors game
+
+import random
+
+random_choice = random.randint(0,2)
+
+print ('The computer chooses:', random_choice)

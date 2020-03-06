be63c09627f50c96d33cc207a49f0dd9e95299d2
Keith Turner <turner@keithturner.com>
2020-03-06 08:42:15 -0700

    Update rock

--- rock.py
+++ rock.py
@@ -1,7 +1,9 @@
 # rock, paper, scissors game
 
+#Import Module (Keep modules at the top of your page)
 import random
 
+#This varible calls the randint function.
 random_choice = random.randint(0,2)
 
 print ('The computer chooses:', random_choice)

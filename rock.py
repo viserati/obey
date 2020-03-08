9ade8da421b91a183145d4d146a700c3e3b04135
Keith Turner <turner@keithturner.com>
2020-03-08 09:43:09 -0600

    Multiline

--- rock.py
+++ rock.py
@@ -1,5 +1,10 @@
-# rock, paper, scissors game.
-
+"""
+----------------------------------------
+This is a multiline comment for the
+rock, paper, scissors game. Not sure why
+it shows up as green in the IDE?
+----------------------------------------
+"""
 #Import Module (Keep modules at the top of your page)
 import random
 

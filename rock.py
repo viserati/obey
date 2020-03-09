c8f62f2c8307f89cc37ba3c8def601357c17cd20
Keith Turner <turner@keithturner.com>
2020-03-08 22:11:30 -0600

    Multiline

--- rock.py
+++ rock.py
@@ -9,6 +9,14 @@ it shows up as green in the IDE?
 import random
 
 #This varible calls the randint function.
-random_choice = random.randint(0,2)
+random_choice = random.randint(0, 2)
 
-print ('The computer chooses:', random_choice)
+if random_choice == 0:
+    computer_choice = "Rock"
+elif random_choice == 1:
+    computer_choice = "Paper"
+else:
+    computer_choice = "Sissors"
+
+user_choice = input('Choose Rock, Paper or Scissors: ')
+print('You chose', user_choice,  'and the Ava chose', computer_choice)

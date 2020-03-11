8866d6d63dd3004bc41a47ee3e45b9fd1bfc3de5
Keith Turner <turner@keithturner.com>
2020-03-10 22:20:56 -0600

    Update RPS

--- rock.py
+++ rock.py
@@ -11,16 +11,26 @@ import random
 winner = ''
 
 #This varible calls the randint function.
-random_choice = random.randint(0, 2)
+random_choice = random.randint(0,2)
 
 if random_choice == 0:
-    computer_choice = "Rock"
+    computer_choice = 'rock'
 elif random_choice == 1:
-    computer_choice = "Paper"
+    computer_choice = 'paper'
 else:
-    computer_choice = "Sissors"
+    computer_choice = 'sissors'
 
 user_choice = input('Choose Rock, Paper or Scissors: ')
 
-if computer_choice == user_choice
+if computer_choice == user_choice:
     winner ='Tie'
+elif computer_choice == 'paper' and user_choice == 'rock':
+    winner = 'Computer'
+elif computer_choice == 'rock' and user_choice == 'scissors':
+    winner = 'Computer'
+elif computer_choice == 'scissors' and user_choice == 'paper':
+    winner = 'Computer'
+else:
+    winner = 'User'
+
+print('The' , winner, 'wins!')

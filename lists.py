51824c7eba3656c7488e5bcd2cdebe8e01021345
Keith Turner <turner@keithturner.com>
2020-03-13 06:40:33 -0600

    snippets

--- lists.py
+++ lists.py
@@ -1,4 +1,4 @@
-# A few exercises with lists
+# A few exercises with lists, AKA Python Arrays
 
 smoothies = ['coconut', 'strawberry', 'banana', 'pineapple', 'acai berry' ]
 
@@ -9,3 +9,9 @@ print('There are', most_recent, 'items in this list')
 recent = smoothies[most_recent -1]
 
 print(recent, 'is the last item in the list')
+
+#----------Enumerate-------------------------------
+
+array_name = ['Item One', 'Item Two', 'Item Three']
+for index, value in enumerate(array_name):
+	print(index, value)
---
+++ snippets.py
@@ -0,0 +1,16 @@
+# A medley of code snippets for Python
+
+# :::::::::::::::::: Boolean :::::::::::::::::
+
+""" In programming, the Boolean data type is
+used to work with statements that can either
+be True or False. Booleans compare two values
+and return whether or not those values are
+similar. """
+
+loyalty_customer = True
+
+if loyalty_customer == True:
+	print("This customer is a Loyalty Customer!")
+else:
+	print("No record.")

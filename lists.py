18ff2f20196916cf1ff63bea88cbb62fd3076b34
Keith Turner <turner@keithturner.com>
2020-03-12 23:18:49 -0600

    lists

--- lists.py
+++ lists.py
@@ -2,4 +2,10 @@
 
 smoothies = ['coconut', 'strawberry', 'banana', 'pineapple', 'acai berry' ]
 
-print(smoothies[2])
+most_recent = len(smoothies)
+
+print('There are', most_recent, 'items in this list')
+
+recent = smoothies[most_recent -1]
+
+print(recent, 'is the last item in the list')

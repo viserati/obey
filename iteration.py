01f6237cbacdcdce1b5782f4384a81ac29b440f7
Keith Turner <turner@keithturner.com>
2020-03-13 20:17:46 -0600

    Iteration

---
+++ iteration.py
@@ -0,0 +1,12 @@
+# Iterating over a list.
+
+scores = [50, 60, 58, 50, 52, 54, 48, 69,34, 61, 55,
+          51, 52, 44, 51, 69, 64, 66, 55, 52,
+          46, 31, 57, 52, 44, 18, 41, 53, 55,
+          61, 51, 44 ]
+
+i = 0
+length = len(scores)
+while i < length:
+    print ('Bubble solution #', i,  'score:', scores[i])
+    i = i + 1

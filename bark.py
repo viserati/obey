a5e688a923cac4a6cc7af821d8495927489ffb4e
Keith Turner <turner@keithturner.com>
2020-03-20 17:09:43 -0600

    Woof!

---
+++ bark.py
@@ -0,0 +1,16 @@
+# An example function
+
+print ("Get those dogs ready!")
+
+def bark(name, weight):
+    if weight > 20:
+        print(name, 'says WOOF WOOF!')
+    else:
+        print(name, 'says woof woof!')
+
+bark('Yoda', 8)
+bark('Codie', 40)
+bark('Sparky', 9)
+bark('Jackson', 65)
+
+print("Okay, we're all done")

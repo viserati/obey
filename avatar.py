009e782c71179e0f2b46c8e6fc8ffd7c0a6f2f68
Keith Turner <turner@keithturner.com>
2020-03-20 21:45:01 -0600

    avatar

---
+++ avatar.py
@@ -0,0 +1,16 @@
+# Creating an avatar
+
+def get_attribute(query, default):
+    question = query + ' [' + default + ']?'
+    answer = input(question)
+    if (answer == ''):
+        answer = default
+        print ('You chose', answer)
+        return answer
+
+hair = get_attribute('what_hair_color', 'brown')
+hair_length = get_attribute('what_hair_length', 'short')
+eye = get_attribute('what_eye_color', 'blue')
+gender = get_attribute('what_gender', 'female')
+glasses = get_attribute('has_glasses', 'no')
+beard = get_attribute('has_beard', 'no')

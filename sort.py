98d1cce2ee6e69f76a7b7304a5bcb8f0c52f9e8f
Keith Turner <turner@keithturner.com>
2020-03-23 04:39:08 -0600

    Bubble Sort

--- sort.py
+++ sort.py
@@ -1,19 +1,28 @@
 # Bubble short
 
-def bubble_sort(scores):
+def bubble_sort(scores, numbers):
     swapped = True
 
     while swapped:
         swapped = False
+        
         for i in range(0, len(scores)-1):
             if scores[i] > scores[i+1]:
                 temp = scores[i]
                 scores[i] = scores[i+1]
                 scores[i+1] = temp
+                temp = numbers[i]
+                numbers[i] = numbers[i+1]
+                numbers[i+1] = temp
                 swapped = True
+
 
 scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69,
           34, 55, 51, 52, 44, 51, 69, 64, 66, 55, 52, 61,
           46, 31, 57, 52, 44, 18, 41, 53, 55, 61, 51, 44 ]
-bubble_sort(scores)
+
+number_of_scores = len(scores)
+solution_numbers = list(range(number_of_scores))
+
+bubble_sort(scores, solution_numbers)
 print(scores)

a2259e7ef63dcbea05a6d2bcd1a70bd130a72284
Keith Turner <turner@keithturner.com>
2020-03-24 12:46:56 -0600

    Analysis

---
+++ analyze.py
@@ -0,0 +1,13 @@
+# Analysis
+
+import ch1text # Notice there is no .py
+
+def compute_readability(text):
+    total_words = 0
+    total_sentences = 0
+    total_syllables = 0
+    score = 0
+
+    print(text)
+
+compute_readability(ch1text.text)

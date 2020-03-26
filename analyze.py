4c55cbf8c6f7dcc65a8f23cc86cba616681cd62e
Keith Turner <turner@keithturner.com>
2020-03-26 08:36:37 -0600

    Syllables

--- analyze.py
+++ analyze.py
@@ -2,10 +2,34 @@
 
 import ch1text # Notice there is no .py
 
+def count_syllables(words):
+    count = 0
+
+    for word in words:
+        word_count = count_syllables_in_word(word)
+        count = count + word_count
+
+    return count
+
+def count_syllables_in_word(word):
+    count = 0
+
+    if len(word) <= 3:
+        return 1
+
+    vowels = 'aeiouAEIOU'
+    for char in word:
+        if char in vowels:
+            count = count + 1
+            
+    return count
+
 def count_sentences(text):
     count = 0
+
+    terminals = '.;?!'
     for char in text:
-         if char == '.' or char ==';' or char == '?' or char == '!':
+         if char in terminals:
              count = count + 1
     return count
 

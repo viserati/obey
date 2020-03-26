547744a6eeb7c9077ccc97c234055baa803e817d
Keith Turner <turner@keithturner.com>
2020-03-25 19:53:47 -0600

    Sentence count

--- analyze.py
+++ analyze.py
@@ -2,6 +2,13 @@
 
 import ch1text # Notice there is no .py
 
+def count_sentences(text):
+    count = 0
+    for char in text:
+         if char == '.' or char ==';' or char == '?' or char == '!':
+             count = count + 1
+    return count
+
 def compute_readability(text):
     total_words = 0
     total_sentences = 0
@@ -10,8 +17,10 @@ def compute_readability(text):
 
     words = text.split()
     total_words = len(words)
+    total_sentences = count_sentences(text)
 
-    print(words)
     print(total_words, 'words')
+    print(total_sentences, 'sentences')
+
 
 compute_readability(ch1text.text)

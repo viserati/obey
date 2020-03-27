d3b9cbc5a6159dc381b80bd219f07551df6fcd52
Keith Turner <turner@keithturner.com>
2020-03-26 22:22:07 -0600

    Readability

--- analyze.py
+++ analyze.py
@@ -14,19 +14,33 @@ def count_syllables(words):
 def count_syllables_in_word(word):
     count = 0
 
-    if len(word) <= 3:
+    endings = '.,;!?:'
+    list_char = word[-1]
+
+    if list_char in endings:
+        processed_word = word[0:-1]
+    else:
+        processed_word = word
+
+    if len(processed_word) <= 3:
         return 1
+
+    if processed_word [-1] in 'eE':
+        processed_word = processed_word[0:-1]
 
     vowels = 'aeiouAEIOU'
     prev_char_was_vowel = False
 
-    for char in word:
+    for char in processed_word:
         if char in vowels:
             if not prev_char_was_vowel:
                 count = count + 1
             prev_char_was_vowel = True
         else:
             prev_char_was_vowel = False
+
+        if processed_word[-1] in 'Yy':
+            count = count + 1
 
     return count
 
@@ -39,6 +53,22 @@ def count_sentences(text):
              count = count + 1
     return count
 
+def output_results(score):
+    if score >= 90:
+        print ('Reading level of 5th Grade')
+    elif score >= 80:
+        print ('Reading level of 6th Grade')
+    elif score >= 70:
+        print ('Reading level of 7th Grade')
+    elif score >= 60:
+        print ('Reading level of 8th-9th Grade')
+    elif score >= 50:
+        print ('Reading level of 10th-12th Grade')
+    elif score >= 30:
+        print ('Reading level of College Student')
+    else:
+        print ('Reading level of College Graduate')
+
 def compute_readability(text):
     total_words = 0
     total_sentences = 0
@@ -50,9 +80,12 @@ def compute_readability(text):
     total_sentences = count_sentences(text)
     total_syllables = count_syllables(words)
 
-    print(total_words, 'words')
-    print(total_sentences, 'sentences')
-    print(total_syllables, 'syllables')
+    score = (206.835 - 1.015 * (total_words / total_sentences) - 84.6 * (total_syllables / total_words))
 
+    # print(total_words, 'words')
+    # print(total_sentences, 'sentences')
+    # print(total_syllables, 'syllables')
+    # print(score, 'reading ease score')
+    output_results(score)
 
 compute_readability(ch1text.text)

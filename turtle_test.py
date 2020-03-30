000a23a7a994ba9e56a36d417a251994837d5773
Keith Turner <turner@keithturner.com>
2020-03-30 12:26:11 -0600

    Turtles 3 shape

--- turtle_test.py
+++ turtle_test.py
@@ -4,16 +4,15 @@ slowpoke = turtle.Turtle()
 slowpoke.shape('turtle')
 slowpoke.color('blue')
 
-slowpoke.pencolor('blue')
-slowpoke.penup()
-slowpoke.setposition(-120, 0)
-slowpoke.pendown()
-slowpoke.circle(50)
+def make_shape(t, sides):
+    angle = 360/sides
+    for i in range(0, sides):
+        t.forward(100)
+        t.right(angle)
 
-slowpoke.pencolor('red')
-slowpoke.penup()
-slowpoke.setposition(-120, 0)
-slowpoke.pendown()
-slowpoke.circle(50)
+make_shape(slowpoke, 3)
+make_shape(slowpoke, 5)
+make_shape(slowpoke, 8)
+make_shape(slowpoke, 10)
 
 turtle.mainloop()

97602a5b97cf0d0f6df73e0a5d02275518cd645b
Keith Turner <turner@keithturner.com>
2020-03-30 11:28:15 -0600

    Turtles

---
+++ turtle_test.py
@@ -0,0 +1,31 @@
+import turtle
+
+slowpoke = turtle.Turtle()
+slowpoke.shape('turtle')
+
+pokey = turtle.Turtle()
+pokey.shape('turtle')
+pokey.color('red')
+
+def make_square(the_turtle):
+    for i in range(0,4):
+        the_turtle.forward(100)
+        the_turtle.right(90)
+
+make_square(slowpoke)
+pokey.right(45)
+make_square(pokey)
+
+
+
+'''
+slowpoke.forward(100)
+slowpoke.right(90)
+slowpoke.forward(100)
+slowpoke.right(90)
+slowpoke.forward(100)
+slowpoke.right(90)
+slowpoke.forward(100)
+slowpoke.right(90)
+'''
+turtle.mainloop()

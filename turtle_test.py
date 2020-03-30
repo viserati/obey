1395d3317340bfef415e4d5c36ab6e67ea567628
Keith Turner <turner@keithturner.com>
2020-03-30 11:38:42 -0600

    Turtles spiral

--- turtle_test.py
+++ turtle_test.py
@@ -2,6 +2,7 @@ import turtle
 
 slowpoke = turtle.Turtle()
 slowpoke.shape('turtle')
+slowpoke.color('blue')
 
 pokey = turtle.Turtle()
 pokey.shape('turtle')
@@ -12,20 +13,13 @@ def make_square(the_turtle):
         the_turtle.forward(100)
         the_turtle.right(90)
 
-make_square(slowpoke)
-pokey.right(45)
-make_square(pokey)
+def make_spiral(the_turtle):
+    for i in range(0, 36):
+        make_square(the_turtle)
+        the_turtle.right(10)
 
+make_spiral(slowpoke)
+pokey.right(5)
+make_spiral(pokey)
 
-
-'''
-slowpoke.forward(100)
-slowpoke.right(90)
-slowpoke.forward(100)
-slowpoke.right(90)
-slowpoke.forward(100)
-slowpoke.right(90)
-slowpoke.forward(100)
-slowpoke.right(90)
-'''
 turtle.mainloop()

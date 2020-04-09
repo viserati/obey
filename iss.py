8ac4795465a0b7eca8c665bcf7237ec0ea6bd55c
Keith Turner <turner@keithturner.com>
2020-04-09 08:41:20 -0600

    Iss lattitude, longitude

--- iss.py
+++ iss.py
@@ -1,12 +1,16 @@
 # ISS (International Space Station) API.
 
-import requests
+import requests, json
 
 url = 'http://api.open-notify.org/iss-now.json'
 
 response = requests.get(url)
 
 if (response.status_code == 200):
-    print (response.text)
+    response_dictionary = json.loads(response.text)
+
+    position = response_dictionary['iss_position']
+    print('International Space Station at ' +
+        position['latitude'] + ', ' + position['longitude'])
 else:
     print('Houston, we have a problem:', response.status_code)

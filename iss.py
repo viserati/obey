a13e9fe1f4a8a1a11567a979e74d796773cec742
Keith Turner <turner@keithturner.com>
2020-04-09 23:07:33 -0600

    Iss final

--- iss.py
+++ iss.py
@@ -2,36 +2,48 @@
 
 import requests, json, turtle
 
+iss = turtle.Turtle()
+
+def setup(window):
+    global iss
+
+    window.setup(1000, 500)
+    window.bgpic('earth.gif')
+    window.setworldcoordinates(-180, -90, 180, 90)
+    turtle.register_shape('iss.gif')
+    iss.shape('iss.gif')
+
+
 def move_iss(lat, long):
     global iss
 
+    iss.hideturtle()
     iss.penup()
     iss.goto(long, lat)
     iss.pendown()
+    iss.showturtle()
 
+def track_iss():
+    url = 'http://api.open-notify.org/iss-now.json'
+    response = requests.get(url)
+    if (response.status_code == 200):
+        response_dictionary = json.loads(response.text)
+        position = response_dictionary['iss_position']
+        lat = float(position['latitude'])
+        long = float(position['longitude'])
+        move_iss(lat, long)
+    else:
+        print('Houston, we have a problem:', response.status_code)
 
-screen = turtle.Screen()
-screen.setup(1000, 500)
-screen.bgpic('earth.gif')
-screen.setworldcoordinates(-180, -90, 180, 90)
+    widget = turtle.getcanvas()
+    widget.after(5000, track_iss)
 
-iss = turtle.Turtle()
-turtle.register_shape('iss.gif')
-iss.shape('iss.gif')
+def main():
+    global iss
+    screen = turtle.Screen()
+    setup(screen)
+    track_iss()
 
-
-url = 'http://api.open-notify.org/iss-now.json'
-
-response = requests.get(url)
-
-if (response.status_code == 200):
-    response_dictionary = json.loads(response.text)
-    position = response_dictionary['iss_position']
-
-    lat = float(position['latitude'])
-    long = float(position['longitude'])
-    move_iss(lat, long)
-else:
-    print('Houston, we have a problem:', response.status_code)
-
-turtle.mainloop()
+if __name__ == "__main__":
+    main()
+    turtle.mainloop()
